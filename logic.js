aa13bd164d3290e89203a44ce4516d72d0888935
unknown <allansichangi35@gmail.com>
2021-11-15 14:52:05 +0300

    working example

--- logic.js
+++ logic.js
@@ -8,3 +8,54 @@ light.onclick = () => {
 		body.classList.replace("light", "dark");
 	}
 };
+
+const form = document.querySelector(".todoItem");
+const submit = document.getElementById("submit");
+const clear = document.getElementById("xlogo");
+const check = document.getElementById("check");
+const input = document.getElementById("createToDo");
+const container = document.querySelector(".topTwo");
+const containerOne = document.querySelector(".secondContainer");
+const list = document.querySelector(".list");
+
+// edit options
+let editing = false;
+
+form.addEventListener("submit", function addItem(ev) {
+	ev.preventDefault();
+
+	const item = input.value;
+	const id = new Date().getTime().toString();
+	if (item && !editing) {
+		const element = document.createElement("div");
+		element.classList.add("topTwo");
+		const attr = document.createAttribute("data-id");
+		attr.value = id;
+		element.setAttributeNode(attr);
+
+		element.innerHTML = `<div class="iconOne">
+		<div class="line">
+			<a id="check"></a>
+		</div>
+		</div>
+		<div class="content">
+			<p class="contentP">${item}</p>
+		</div>
+		<div class="icon">
+			<a id="xlogo"></a>
+		</div>`;
+		list.appendChild(element);
+		containerOne.classList.add("topShow");
+
+		setDefautlt();
+	} else if (item && editing) {
+		console.log("not true");
+	} else {
+		console.log("empty");
+	}
+});
+
+function setDefautlt() {
+	input.value = "";
+	editing = false;
+}
